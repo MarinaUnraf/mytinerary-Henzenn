344c8daf6064fdfd05ed201e0fbd0ab2275de227
MarinaUnraf <marina.henzenn@unraf.edu.ar>
2023-07-31 16:35:53 -0300

    Creating node project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+//import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+     <header>
+
+     </header>
+     <body>
+      
+     </body>
+     <footer>
+
+     </footer>
+    </>
+  )
+}
+
+export default App
