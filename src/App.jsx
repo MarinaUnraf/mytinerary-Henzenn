01d2452c21f103f245daf1be5b425402b4d1c038
MarinaUnraf <marina.henzenn@unraf.edu.ar>
2023-08-01 15:09:51 -0300

    first format

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 //import { useState } from 'react'
-
 import './App.css'
 
 function App() {
   
   return (
     <>
-     <header>
-
+     <header className='header'>
+      <h1>MyTinerary</h1>
+      <nav className='Nav'>
+            <a href="">Home</a>
+            <a href="">Cities</a>
+            <button>Login</button>
+      </nav>
      </header>
-     <body>
-      
-     </body>
-     <footer>
+     <main className='main'>
 
+     </main>
+     <footer className='footer'>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ratione debitis molestiae aut consequatur aliquid dignissimos sequi, voluptatem temporibus perspiciatis tenetur, iure voluptatibus necessitatibus corporis. Ducimus illo iusto iure deserunt!</p>
      </footer>
     </>
   )
