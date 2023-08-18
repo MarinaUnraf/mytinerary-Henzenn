a8afd29cf595bb9b9e5e828c66172ae2d0134b04
MarinaUnraf <marina.henzenn@unraf.edu.ar>
2023-08-18 16:38:22 -0300

    linked call to action

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,8 @@ const router = createBrowserRouter([
 
       {path: '/', element: <Layout/>,
         children:[{path: '/', element: <Index/>},
-                  {path: '/cities', element: <Cities/>}
+                  {path: '/cities', element: <Cities/>},
+                  {path: '/cities/:id', element: <Cities/>}
       ]
     }
 ])
